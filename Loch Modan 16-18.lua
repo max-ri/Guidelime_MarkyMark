886544e8066d602654d68e0371aba53848cdecb9
max-ri <51713394+max-ri@users.noreply.github.com>
2019-07-17 19:39:25 +0200

    addon created; added guides 12-16, 16-18

---
+++ Loch Modan 16-18.lua
@@ -0,0 +1,53 @@
+local addonName, addon = ...
+Guidelime.registerGuide([[
+[DMarkyMark's 1-60 guide\\https://www.twitch.tv/darealmarkymark\\(best suited for a group of at least 2, if not 3+)]
+[N16-18Loch Modan]
+SAVE ALL GOOEY SPIDER LEGS & CRISP SPIDER MEAT[O]
+[S] Set Hearth in Thelsamar
+Run into the basement hut, accept [G37.24,47.38Loch Modan][QA436 Ironband's Excavation]
+Go do [G32.9,85.01][QC237 In Defense of the King's Lands Part 2] if you haven't done so already, [G23.54,76.4][QT237 turn it in] & accept [G23.47,74.51][QA263 Part 3]
+Grind all the way along the south end of the lake to Ironband's Digsite killing all mobs on the way[G66,65]
+Turn in [G64.9,66.66][QT436 Ironband's Excavation], accept [QA297 Gathering Idols]
+Accept [G65.93,65.62][QA298 Excavation Progress Report]
+Kill the Troggs nearby for [G70.09,64.2][QC297 Gathering Idols] - look out for Magosh
+Turn in [G64.9,66.66][QT297 Gathering Idols]
+Buy Solstice Robe and Wise Man's Belt from vendor [A Mage,Warlock,Priest]
+Grind all mobs all the way to the Farstrider Lodge[G82,62]
+Accept [G83.47,65.45][QA257 A Hunter's Boast]
+Accept [G81.76,61.66][QA385 Crocolisk Hunting]
+Go kill 6 Buzzards south of the Lodge for [G75.07,72.54][QC257 A Hunter's Boast], then return to the Lodge
+Turn in [G83.47,65.45][QT257 A Hunter's Boast], accept [QA258 A Hunter's Challenge]
+Go kill the 5 boars for [G63.93,38.67][QC258 A Hunter's Challenge], then return to the Lodge
+Turn in [G83.47,65.45][QT258 A Hunter's Challenge], accept [G81.73,64.14][QA271 Vyrin's Revenge]
+Grind to Bingles near the lake[G63.6, 48]
+Turn in [G63.56,47.92][QT2039 Find Bingles], accept [QA2038 Bingles' Missing Supplies]
+Clear up the coast until you reach the big island with Crocs & finish [G56.61,38.08][QC385 Crocolisk Hunting]
+Go up to the north islands and do [G50.77,25.32][QC2038 Bingles' Missing Supplies] and [G35.86,91.83][QC263 In Defense of the King's Lands Part 3]
+Once both quests are complete, grind up to the Dam[OC]
+Accept [G46.05,13.61][QA250 A Dark Threat Looms]
+Go to the east side of the dam and turn in [G56.05,13.24][QT250 A Dark Threat Looms] accept [QA199 Part 2]
+Turn in [G46.05,13.61][QT199 Part 2], accept [QA161 A Dark Threat Looms Part 3]
+[H] Hearth to Thelsamar
+Turn in [G37.24,47.38][QT298 Excavation Progress Report], accept [QA301 Report to Ironforge]
+Run to the south tower and turn in [G23.47,74.51][QT263 In Defense of the King's Lands Part 3]
+Accept [G23.23,73.68][QA217 In Defense of the King's Lands Part 4]
+Go to the cave at 34.7, 84.5 and do [QC217 In Defense of the King's Lands Part 4][G34.7, 84.5]
+Turn in [G23.23,73.68][QT217 In Defense of the King's Lands Part 4]
+Walk through the Trogg camp, find & kill Ol' Sooty for [G37.92,63.38][QC271 Vyrin's Revenge]
+Grind along the lake until you reach Bingles. Turn in [G63.56,47.92][QT2038 Bingles' Missing Supplies]
+Grind east until you reach the Hunters Lodge. Turn in [G81.76,61.66][QT385 Crocolisk Hunting] & [G83.47,65.45][QT271 Vyrin's Revenge], accept [QA531 2nd part] and [G81.73,64.14][QT531 turn it in]
+Should be very close to 18 now or already 18. Grind until [XP18].
+Once 18, die and spirit rez at Thelsamar. [F] Fly to Ironforge.
+[T] Train class abilities, INCLUDING LOCKPICKING AND BUY THIEVES TOOLS. LEVEL & TRAIN COOKING TO 50
+Turn in [QT301 Report to Ironforge]
+Accept [QA2041 Speak with Shoni]
+Take the tram to Stormwind[OC]
+ERROR Turn in [QT2041 Speak with Shoni], skip [QS? next part]
+Turn in [QT1338 Stormpike's Order]
+Rogue only, accept [QA2281 Redridge Rendezvous][A Rogue]
+Buy Stormwind Seasoning Herbs[G61,61]
+Accept [QA3765 The Corruption Abroad] in The Park[G21,56]
+From here, run directly to Goldshire[OC]
+[S] Set hearth in Goldshire[G64 ,61]
+Run east to Redridge
+]], GetAddOnMetadata(addonName, "title"))
