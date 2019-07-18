f7de5b336f7f0d6706072cbad814f267cb26b08e
max-ri <max_r_@web.de>
2019-07-18 12:48:42 +0200

    added 5-11 Elwynn Forest

--- Dun Morogh_Loch Modan 11-12.lua
+++ Dun Morogh_Loch Modan 11-12.lua
@@ -3,9 +3,9 @@ Guidelime.registerGuide([[
 [D*MarkyMark*'s 1-60 guide\https://www.twitch.tv/darealmarkymark\(best suited for a group of at least 2, if not 3+)]
 [GA Alliance]
 [N11-12Dun Morogh/Loch Modan]
-[NX12-16Loch Modan]
+[NX12-16Darkshore]
 Accept [QA314 Protecting the Herd][G63.1, 49.9 Dun Morogh]
-Kill Vagash and loot, turn in [G63.08,49.85Dun Morogh][QT314 Protecting the Herd]
+Kill [G62.62,46.05Dun Morogh][QC314,1 Vagash] and loot, turn in [G63.08,49.85][QT314 Protecting the Herd]
 Run south to Gol'Balar Quarry[OC]
 Accept [QA433 The Public Servant][G68,56]
 Accept [QA432 Those Blasted Troggs!][G68,56]
@@ -27,13 +27,13 @@ Accept [G24.76,18.39][QA1338 Stormpike's Order]
 Accept [G24.76,18.39][QA307 Filthy Paws]
 Grind east to Silver Stream Mine[OC]
 Do [G36.76,20.6][QC416 Rat Catching] & [G35.48,24.36][QC307 Filthy Paws] 
-Check the vendor at the Dam while here for 'Heavy Spiked Mace' - cost is 73s 50c each
+Check the [V] vendor at the Dam while here for 'Heavy Spiked Mace' - cost is 73s 50c each
 Turn in [G24.76,18.39][QT307 Filthy Paws]
 Run through the North Gate Pass[OC]
 Accept [G83.89,39.19Dun Morogh][QA419 The Lost Pilot]
 Run to the corpse and turn in [QT419 The Lost Pilot][G80, 36]
 Accept [G79.67,36.17][QA417 A Pilot's Revenge]
-Kill Mangeclaw for [G78.31,37.76][QC417 A Pilot's Revenge]
+Kill [G78.31,37.76][QC417,1 Mangeclaw] for [QC417 A Pilot's Revenge]
 Run back to Pilot Hammerfoot[OC]
 Turn in [G83.89,39.19][QT417 A Pilot's Revenge]
 Grind to Thelsamar[OC]
@@ -42,11 +42,11 @@ Turn in [G34.26,47.71][QT416 Rat Catching]
 Turn in [G37.02,47.81][QT6392 Return to Brock][A Dwarf,Gnome]
 [H] Hearth to Ironforge
 Put your 'Boar Intestines', 'Bear Meat' & 'Spider Ichor' in the bank if you didn't finish Blood Sausages yet
-Purchase 'Bronze Tube' from the Engineering Vendor if they're available[G68,43 Ironforge]
-[T] Train Cooking, buy 'Simple Wood' & 'Flint and Tinder'
+[V] Purchase 'Bronze Tube' from the Engineering Vendor if they're available[G68,43 Ironforge]
+[T] Train Cooking, [V] buy 'Simple Wood' & 'Flint and Tinder'
 [T] Train First Aid
 [T] Train class abilities
-Go to the General Goods Vendor in Ironforge and buy 'Simple Wood', 'Flint and Tinder' and bags[G37, 76]
+Go to the General Goods Vendor in Ironforge and [V] buy 'Simple Wood', 'Flint and Tinder' and bags[G37, 76]
 Run to [G31,34 Dun Morogh] in Dun Morogh. Walk up the mountain, die in the correct spot & spirit res in Menethil Harbor
 Grab the flightpath[P]
 Take the boat to Darkshore
