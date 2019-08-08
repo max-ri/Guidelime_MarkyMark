34a31c1d9beb321d3c9bf94efb6c8e1f015138e4
max-ri <max_r_@web.de>
2019-08-08 14:00:19 +0200

    some errors fixed

--- Elwynn Forest 5-11.lua
+++ Elwynn Forest 5-11.lua
@@ -10,18 +10,18 @@ Accept [G34.65,84.48Elwynn Forest][QA88 Princess Must Die] if level [XP6] alread
 Grind east[OC] to the Maclure Vineyard
 [V Vendor], make sure to save all 'Chunk of Boar Meat'
 Accept [G43.15,89.62Elwynn Forest][QA106 Young Lovers]
-Turn in [QC85 Lost Necklace] & accept [G43.13,85.72Elwynn Forest][QA86 Pie for Billy]
+Turn in [G43.13,85.72Elwynn Forest][QT85 Lost Necklace] & accept [G43.13,85.72Elwynn Forest][QA86 Pie for Billy]
 Grind West[OC], through the kobolds, to Stonefield Farm
-Turn in [G50.77,80.18Elwynn Forest][QC86 Pie for Billy] & accept [G34.49,84.25Elwynn Forest][QA84 Back to Billy]
-Turn in [QC106 Young Lovers] near the river & accept [G29.84,86Elwynn Forest][QA111 Speak with Gramma]
-Turn in [QC111 Speak with Gramma] & accept [G34.94,83.86Elwynn Forest][QA107 Note to William]
+Turn in [G34.49,84.25Elwynn Forest][QT86 Pie for Billy] & accept [G34.49,84.25Elwynn Forest][QA84 Back to Billy]
+Turn in [G29.84,86Elwynn Forest][QT106 Young Lovers] near the river & accept [G29.84,86Elwynn Forest][QA111 Speak with Gramma]
+Turn in [G34.94,83.86Elwynn Forest][QT111 Speak with Gramma] & accept [G34.94,83.86Elwynn Forest][QA107 Note to William]
 Grind back east to the Maclure Vineyard
-Turn in [QC84 Back to Billy], accept [G43.13,85.72Elwynn Forest][QA87 Goldtooth]
+Turn in [G43.13,85.72Elwynn Forest][QT84 Back to Billy], accept [G43.13,85.72Elwynn Forest][QA87 Goldtooth]
 Grind west to the mines, clear through the lower entrance to [G41.71,78.04Elwynn Forest][QC87,1 Goldtooth], finish [G48.14,71.15Elwynn Forest][QC60 Kobold Candles] & [G48.14,71.15Elwynn Forest][QC47 Gold Dust Exchange]
 Hearth to [H Goldshire]
 Turn in [G43.32,65.7Elwynn Forest][QT60 Kobold Candles] & [G43.32,65.7Elwynn Forest][QT107 Note to William]
 Accept [G43.32,65.7Elwynn Forest][QA61 Shipment to Stormwind] & [G43.32,65.7Elwynn Forest][QA112 Collecting Kelp]
-Turn in [G48.14,71.15Elwynn Forest][QC47 Gold Dust Exchange], accept [G42.14,67.26Elwynn Forest][QA40 A Fishy Peril]
+Turn in [G42.14,67.26Elwynn Forest][QT47 Gold Dust Exchange], accept [G42.14,67.26Elwynn Forest][QA40 A Fishy Peril]
 Turn in [G42.11,65.93Elwynn Forest][QT40 A Fishy Peril] & [G42.11,65.93Elwynn Forest][QT62 The Fargodeep Mine]
 Accept [G42.11,65.93Elwynn Forest][QA35 Further Concerns] & [G42.11,65.93Elwynn Forest][QA76 The Jasperlode Mine]
 [V Buy] new weapons from the [V vendor]
@@ -33,7 +33,7 @@ Grind to Eastvale Logging Camp
 Accept [G81.38,66.11Elwynn Forest][QA5545 A Bundle of Trouble] & [G79.46,68.79Elwynn Forest][QA83 Red Linen Goods]
 Grind northwest until you reach 72,60 and turn in [G72.65,60.34Elwynn Forest][QT37 Find the Lost Guards] [G72,60Elwynn Forest]
 Accept [G72.65,60.34Elwynn Forest][QA45 Discover Rolf's Fate]
-Grind east until you reach Rolf's Corpse, turn in [QC45 Discover Rolf's Fate] & accept [G79.79,55.5Elwynn Forest][QA71 Report to Thomas] [G80,55.6Elwynn Forest]
+Grind east until you reach Rolf's Corpse, turn in [G79.79,55.5Elwynn Forest][QT45 Discover Rolf's Fate] & accept [G79.79,55.5Elwynn Forest][QA71 Report to Thomas] [G80,55.6Elwynn Forest]
 Swim to the middle of Stonecairn Lake and kill Defias for [G58.45,67.91Elwynn Forest][QC83 Red Linen Goods]
 Grind south again, kill [G80.51,66.66Elwynn Forest][QC52,1 Prowlers]/Bears & loot Wood until [G74.33,69.72Elwynn Forest][QC52 Protect the Frontier] & [G79.88,59.46Elwynn Forest][QC5545 A Bundle of Trouble] are done
 Turn in [G79.46,68.79Elwynn Forest][QT83 Red Linen Goods] & [G81.38,66.11Elwynn Forest][QT5545 A Bundle of Trouble] in Eastvale Logging Camp
@@ -43,29 +43,29 @@ Grind all the way west to Goldshire
 Turn in [G42.11,65.93Elwynn Forest][QT39 Deliver Thomas' Report] & [G42.11,65.93Elwynn Forest][QT76 The Jasperlode Mine]
 Accept [G42.11,65.93Elwynn Forest][QA239 Westbrook Garrison Needs Help]
 Accept [QA1097 Elmore's Task]
-Turn in [G54.27,66.27Elwynn Forest][QC112 Collecting Kelp] in the inn (have to wait 5 seconds to get followup)
+Turn in [G43.32,65.7Elwynn Forest][QT112 Collecting Kelp] in the inn (have to wait 5 seconds to get followup)
 Accept [G43.32,65.7Elwynn Forest][QA114 The Escape]
 Run south to the Maclure Vineyard
 Turn in [G43.15,89.62Elwynn Forest][QT114 The Escape] [G43,89Elwynn Forest]
 Run to the Stonefield Farm and turn in [G34.65,84.48Elwynn Forest][QT88 Princess Must Die!]
 Turn in [G34.49,84.25Elwynn Forest][QT87 Goldtooth]
 Grind west[OC] to Westbrook Garrison
-Turn in [QC239 Westbrook Garrison Needs Help] & accept [G24.23,74.45Elwynn Forest][QA11 Riverpaw Gnoll Bounty] [G24,74Elwynn Forest]
+Turn in [G24.23,74.45Elwynn Forest][QT239 Westbrook Garrison Needs Help] & accept [G24.23,74.45Elwynn Forest][QA11 Riverpaw Gnoll Bounty] [G24,74Elwynn Forest]
 Accept [G24.56,76.45Elwynn Forest][QA176 Wanted: Hogger]
 Grind south of Westbrook Garrison, kill Gnolls for [G46.54,66.5Elwynn Forest][QC11 Riverpaw Gnoll Bounty]
 Kill & loot [G26.83,86.69Elwynn Forest][QC176,1 Hogger]
 Turn in [G24.23,74.45Elwynn Forest][QT11 Riverpaw Gnoll Bounty]
-Grind west into Westfall, turn in [QC184 Furlbrow's Deed] if looted, accept [G59.92,19.42Westfall][QA36 Westfall Stew] [G60,19 Westfall]
-Grind to Saldean's Farm and turn in [QC36 Westfall Stew], skip the [QS38 next part]
+Grind west into Westfall, turn in [G59.95,19.36Westfall][QT184 Furlbrow's Deed] if looted, accept [G59.92,19.42Westfall][QA36 Westfall Stew] [G60,19 Westfall]
+Grind to Saldean's Farm and turn in [G56.42,30.51Westfall][QT36 Westfall Stew], skip the [QS38 next part]
 Continue grinding west till Sentinel Hill, turn in [QT109 Report to Gryan Stoutmantle] [G56,47 Westfall]
-Accept [QC6181 A Swift Message] inside the tower, turn [G56.55,52.64Westfall][QT6181 it] in at the Gryphon Master & accept [QT6281 Continue to Stormwind]
+Accept [G56.99,47.17Westfall][QA6181 A Swift Message] inside the tower, turn [G56.55,52.64Westfall][QT6181 it] in at the Gryphon Master & accept [QT6281 Continue to Stormwind]
 Hearth to [H Goldshire]
-Turn in [G26.83,86.69Elwynn Forest][QC176 Wanted: Hogger], accept level [XP 10] class quests
+Turn in [G42.11,65.93Elwynn Forest][QT176 Wanted: Hogger], accept level [XP 10] class quests
 Run north to Stormwind, turn in [QT61 Shipment to Stormwind]
 [V Buy] weapon [T skills] & new weapons
 Turn in [QT6281 Continue to Stormwind] in Oldtown
 Accept [QA6261 Dungar Longdrink]
-Turn in [QC6261 Dungar Longdrink] at the flightmaster, accept [QA6285 Return to Lewis]
+Turn in [QT6261 Dungar Longdrink] at the flightmaster, accept [QA6285 Return to Lewis]
 Fly to [F Westfall], turn in [G56.99,47.17Westfall][QT6285 Return to Lewis]
 [F]Fly back to Stormwind
 Head to Dwarven District and turn in [G51.75,12.07Stormwind City][QT1097 Elmore's Task]
@@ -73,7 +73,7 @@ Accept [G51.75,12.07Stormwind City][QA353 Stormpike's Delivery]
 Check the Engineering [V Vendor] at [G55,7Elwynn Forest] and [V buy] Bronze Tubes if in stock
 Take the tram to Ironforge
 Check the Engineering [V Vendor] at [G68,46Elwynn Forest] and [V buy] Bronze Tubes if in stock
-Get the FP
+Get the [P]FP
 [T Train] Cooking
 [S]Set Hearth to [H Ironforge]
 Grind to Amberstill Ranch in Dun Morogh
